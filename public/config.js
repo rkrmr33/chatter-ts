95236857f562018ae5f6bec0f4fcd93ddc7d16b7
roikramer120 <roikramer120@gmail.com>
2019-02-06 21:34:51 +0200

    added dynamic port control

---
+++ public/config.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var env = process.env;
+exports.default = {
+    PORT: env.PORT || 3000,
+    HOST: env.PORT || '0.0.0.0'
+};
--- public/server.js
+++ public/server.js
@@ -4,10 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var config_1 = __importDefault(require("./config"));
 var server = express_1.default();
 server.get('/', function (req, res) {
     res.send('Hello world');
 });
-server.listen(3000, function () {
-    console.log('server is listening on port 3000...');
+server.listen(config_1.default.PORT, function () {
+    console.log("server is listening on port " + config_1.default.PORT + "...");
 });
