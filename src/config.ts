d15ec164e7b374d642f19b5d4793cd52d379b2a9
roikramer120 <roikramer120@gmail.com>
2019-02-06 21:32:00 +0200

    added dynamic port

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+const env = process.env;
+
+export default {
+  PORT: env.PORT || 3000,
+  HOST: env.PORT || '0.0.0.0'
+}
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
+import config from './config';
+
 const server = express();
 
 server.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-server.listen(3000, () => {
-  console.log('server is listening on port 3000...');
+server.listen(config.PORT, () => {
+  console.log(`server is listening on port ${config.PORT}...`);
 });
