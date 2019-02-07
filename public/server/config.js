788d8f8b18ae566e4acdabafbbb5e5c26cdefd07
roikramer120 <roikramer120@gmail.com>
2019-02-07 22:03:32 +0200

    The fetch fix... it was the config HOST variable was eccidentaly set to env.PORT

--- public/server/config.js
+++ public/server/config.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var env = process.env;
 exports.default = {
     PORT: env.PORT || 3000,
-    HOST: env.PORT || '0.0.0.0',
+    HOST: env.HOST || '0.0.0.0',
     DB_CONN_STR: 'mongodb://root:wonder100@ds115553.mlab.com:15553/heroku_994wtpk0',
     DB_NAME: 'heroku_994wtpk0',
     get endpoint() {
